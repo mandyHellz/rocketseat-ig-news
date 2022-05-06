962a8599879f33e6bfe08887e179995eb418ad59
Amanda Nunes <77729852+mandyHellz@users.noreply.github.com>
2022-05-06 17:52:53 -0300

    feat: create a post page by slug

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,77 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { RichText } from "prismic-dom";
+import { GetPrismicClient } from "../../services/prismic";
+import Head from "next/head";
+import styles from "./post.module.scss";
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            dangerouslySetInnerHTML={{ __html: post.content }}
+            className={styles.postContent}
+          />
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const session = await getSession({ req });
+  const slug = params?.slug;
+
+  const prismic = GetPrismicClient(req);
+
+  const response = await prismic.getByUID<any>("post", String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  //   if (slug === "favicon.png") {
+  //     return {
+  //       redirect: {
+  //         destination: "/",
+  //         permanent: false,
+  //       },
+  //     };
+  //   }
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,6 +4,7 @@ import Prismic from "@prismicio/client";
 import { GetStaticProps } from "next";
 import { GetPrismicClient } from "../../services/prismic";
 import { RichText } from "prismic-dom";
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -25,11 +26,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
